9394a351433d74e8bc6db260dbed2282c48de763
kakabisht <kakabisht07@gmail.com>
2020-07-07 23:51:33 +0530

    Working with Admin interface and templates, customizing adming dashboad

---
+++ my_video_rental/videos/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+from . import models
+class MovieAdmin(admin.ModelAdmin):
+    fields=['release_year','title','length']
+
+    search_fields=['title']
+
+    list_filter=['release_year','length']
+
+    list_display=['release_year','title','length']
+
+    list_editable=['length']
+
+
+admin.site.register(models.Customer)
+admin.site.register(models.Movie,MovieAdmin)
+
+
+    
